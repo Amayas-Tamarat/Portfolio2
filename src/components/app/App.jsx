cd7b60baddbdebd671fe15d4ea9b523aa925a10f
Amayas <amayas.tamarat@gmail.com>
2025-07-07 14:50:22 +0200

    setup gsap tailwind

---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import '../navbar/Navbar.jsx'
+import Navbar from "../navbar/Navbar.jsx";
+
+function App() {
+
+  return (
+      <>
+          <Navbar />
+      </>
+  )
+}
+
+export default App
